9205102e61a5da9d1d223eab56d087bb111ffa73
johnnyw <jwu38438@gmail.com>
2024-10-01 13:58:05 -0400

    egerregergerg

---
+++ index.js
@@ -0,0 +1,10 @@
+const DOMSelectors = {
+  type: document.querySelector(".Name"),
+  name: document.querySelector(".Type"),
+  weight: document.querySelector(".Weight"),
+  form: document.querySelector(".form"),
+  button: document.querySelector(".submit"),
+};
+DOMSelectors.button.addEventListener("click", () => function test() {});
+
+/* event.preventDefault = no refresh*/
