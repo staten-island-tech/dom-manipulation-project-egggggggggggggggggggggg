16d56b12f4bc00e54a3efe608086a2afabd00340
johnnyw <jwu38438@gmail.com>
2024-10-09 13:56:15 -0400

    sigma

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const DOMSelectors = {
-  type: document.querySelector(".Name").value,
-  name: document.querySelector(".Type").value,
-  weight: document.querySelector(".Weight").value,
   form: document.querySelector("form"),
   button: document.querySelector(".btn"),
+  container: document.querySelector(".container"),
 };
 
-console.log(DOMSelectors.button);
+console.log(DOMSelectors);
 /* event.preventDefault = no refresh*/
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
+  const type = document.querySelector(".Type").value;
+  const name = document.querySelector(".Name").value;
+  const weight = document.querySelector(".Weight").value;
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `               <div class="card">
+    <h2 class="card_header">${name}</h2>
+    <h2 class="stats">${weight}</h2>
+    <h2 class="type">${type}</h2>
+    <img src=${weight} alt=${weight}>
+</div>
+  `
+  );
 });
